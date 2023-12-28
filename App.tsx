b8d3cd8f3208246fda05720078d85ff8bdb0eae0
Kurt Miller <kmiller9393@gmail.com>
2023-12-28 16:41:28 -0700

    initial App component setup

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { FlyMap } from './src/components/FlyMap';
+
+// map 
+  // shows past fly fishing spots, save them so they persist when closing and reopening app
+  // make sure user can get directions from their current location to each pin
+
+  // Also want to add tab navigation:
+    // Screen 1: Home Screen with map on it
+    // Screen 2: Basic Account Screen for now
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <FlyMap />
     </View>
   );
 }
